994ef24255a85502c96a2671b7a6c71cca8cc9e9
MuhammadSoaleh <126265837+MuhammadSoaleh@users.noreply.github.com>
2023-11-08 23:49:31 +0500

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,52 @@
 
+.primary-btn {
+  /* display: inline-block; */
+  font-size: 15px;
+  font-family: "Play", sans-serif;
+  font-weight: 700;
+  padding: 14px 32px 12px;
+  color: #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  position: relative;
+  z-index: 1;
+}
+.primary-btn:hover:before {
+  height: 100%;
+  width: 100%;
+}
+.primary-btn:hover:after {
+  height: 100%;
+  width: 100%;
+}
+.primary-btn:before {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 30px;
+  width: 30px;
+  border-left: 2px solid rgba(213, 170, 109, 1);;
+  border-top: 2px solid rgba(213, 170, 109, 1);;
+  content: "";
+  z-index: -1;
+  -webkit-transition: all, 0.7s;
+  -o-transition: all, 0.7s;
+  transition: all, 0.7s;
+}
+.primary-btn:after {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  height: 30px;
+  width: 30px;
+  border-right: 2px solid rgba(213, 170, 109, 1);;
+  border-bottom: 2px solid rgba(213, 170, 109, 1);;
+  content: "";
+  z-index: -1;
+  -webkit-transition: all, 0.7s;
+  -o-transition: all, 0.7s;
+  transition: all, 0.7s;
+}
 
 
 
